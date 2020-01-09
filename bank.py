947ff3434064f206a99431b2640ff725504d160f
Leandro Tavares <leandro.mendestavares@gmail.com>
2020-01-09 16:14:07 -0300

    Add aula Herança

--- bank.py
+++ bank.py
@@ -1,9 +1,9 @@
 class Account:
 
-    def __init__(self, number, total):
+    def __init__(self, number):
         self.__number = number
         # private self.total = total
-        self.__total = total
+        self.__total = 0
 
     def deposit(self, value):
         self.__total += value
@@ -11,6 +11,23 @@ class Account:
     def withdraw(self, value):
         if self.__total >= value:
             self.__total -= value
+            self.__total -= 1
 
     def get_total(self):
         return self.__total
+
+
+class Account2(Account):
+
+    def __init__(self, number, cvv):
+        super(Account2, self).__init__(number)
+        self.__cvv = cvv
+
+    def withdraw(self, value):
+        # _Account_total - Acesso o atributo privado da classe pai
+        if self._Account__total >= value:
+            self._Account__total -= value
+            self._Account__total -= 2
+
+    def get_cvv(self):
+        return self.__cvv
--- bank_teste.py
+++ bank_teste.py
@@ -1,6 +1,6 @@
 import bank
 
-b = bank.Account('123-12', 50)
+# b = bank.Account('123-12')
 # print(b.number)
 # print(b.total)
 # print()
@@ -21,6 +21,20 @@ b = bank.Account('123-12', 50)
 # print(b.get_total())
 
 # COM MODIFICADORES DE ACESSO
-b.withdraw(200)
-print(b.get_total())
+c1 = bank.Account('123-12')
+c1.deposit(250)
+c1.withdraw(200)
+print(c1.get_total())
 
+
+# DETALHAMENTO DA CLASSE
+print(type(c1))
+print(c1.__class__)
+
+
+# HERANÇA
+c2 = bank.Account2('321-21', 123)
+c2.deposit(250)
+c2.withdraw(200)
+print(c2.get_cvv())
+print(c2.get_total())
